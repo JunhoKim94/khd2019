aa1c5eac2d37a695edcfa35ae59186a1615feb56
junho <junho9000@naver.com>
2019-12-01 18:52:26 +0900

    last_version

--- Fundus/dataprocessing.py
+++ Fundus/dataprocessing.py
@@ -10,18 +10,20 @@ import tensorflow as tf
 import keras
 from multiprocessing import Pool
 from tqdm import tqdm
+from keras_preprocessing import image as ksimg
 
 
 def image_preprocessing(image, target_resolution, normalize):
+# def image_preprocessing(image, rescale, resize_factor):
+    
     if image.shape[:2] != target_resolution:
         res = cv2.resize(image,
                          target_resolution,
                          interpolation=cv2.INTER_AREA)
 
-    '''   
-     if normalize == True:
-        res = res / 255.
-    '''
+    if normalize == True:
+        res = (res / 255.).astype(np.float32)
+
 
     return res
 
@@ -42,29 +44,30 @@ def Label2Class(label):
     return resvec
 
 
+def map_fn(x):
+    return image_preprocessing(cv2.imread(x, 3), (390, 307), True)
+
+
 def dataset_loader(img_path,
                    target_resolution,
                    normalize):
     t1 = time.time()
 
+    
     ## 이미지 읽기
     p_list = [os.path.join(dirpath, f) for dirpath, dirnames, files in os.walk(img_path) for f in files if all(s in f for s in ['.jpg'])]
     p_list.sort()
 
-    images = []
+    with Pool(32) as p:
+        images = p.map(map_fn, tqdm(p_list))
+    
+    print("Mapping_Finished")
+    
     labels = []
-    for i, p in enumerate(tqdm(p_list)):
-        im = cv2.imread(p, 3)
-        im = image_preprocessing(im,
-                                 target_resolution,
-                                 normalize)
-        images.append(im)
-        
-        # label 데이터 생성
-        l = Label2Class(p.split('/')[-2])
-        labels.append(l)
+    for i, p in enumerate(p_list):
+        labels.append(Label2Class(p.split('/')[-2]))
 
-    images = (np.array(images)/255.).astype(np.float32)
+    images = np.array(images).astype(np.float32)
     labels = np.array(labels)
 
     t2 = time.time()
@@ -73,110 +76,3 @@ def dataset_loader(img_path,
     print('Labels:', labels.shape, ' among 0-3 classes')
 
     return images, labels
-
-
-def cutmix(x, y):
-    x = x.copy()
-    y = y.copy()
-    
-    batch_size, height, width, channel = x.shape
-    
-    lmbda = np.random.uniform()
-    sqrt_lmbda = np.sqrt(lmbda)
-    #rand_index = np.random.permutation(batch_size)
-
-    offset_x = int(width * sqrt_lmbda)
-    offset_y = int(height * sqrt_lmbda)
-
-    left = np.random.randint(0, width-offset_x+1)
-    right = left + offset_x
-    down = np.random.randint(0, height-offset_y+1)
-    up = down + offset_y
-    
-    # input reconstruction
-    permutation = np.random.permutation(batch_size)
-    x[:, down:up, left:right] = x[permutation, down:up, left:right]
-
-    # label reconstruction
-    lmbda = (right-left)*(up-down)/(height*width)
-    
-    y = (1-lmbda)*y + lmbda*y[permutation]
-    
-    return x, y
-
-
-def make_dataset(dataset, batch_size=32):
-    dataset = dataset.shuffle(buffer_size=4096)
-    dataset = dataset.batch(batch_size=batch_size,
-                            drop_remainder=True)
-    dataset = dataset.map(map_func=cutmix,
-                          num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
-    return dataset
-    
-
-def cutmix_gen(x, y, batch_size=32):
-    total = x.shape[0]
-    xs, ys = [], []
-    
-    for i in range(total//batch_size):
-        x_batch = x[i*batch_size:(i+1)*batch_size]
-        y_batch = y[i*batch_size:(i+1)*batch_size]
-        yield cutmix(x_batch, y_batch)
-
-
-def apply_cutmix(x, y, batch_size=32):
-    total, height, width, chan = x.shape
-    new_total = total - (total % batch_size)
-    xs = np.zeros(shape=(new_total, height, width, chan),
-                  dtype=np.float32)
-    ys = np.zeros(shape=(new_total, y.shape[-1]),
-                  dtype=np.float32)
-
-    for i in range(total//batch_size):
-        x_batch = x[i*batch_size:(i+1)*batch_size]
-        y_batch = y[i*batch_size:(i+1)*batch_size]
-        x_batch, y_batch = cutmix(x_batch, y_batch)
-        
-        xs[i*batch_size:(i+1)*batch_size] = x_batch
-        ys[i*batch_size:(i+1)*batch_size] = y_batch
-        
-    return xs, ys
-
-
-class CutmixGen(keras.utils.Sequence):
-    def __init__(self, x_set, y_set, batch_size):
-        self.x, self.y = x_set, y_set
-        self.batch_size = batch_size
-
-    def __len__(self):
-        return int(len(self.x) / self.batch_size)
-
-    def __getitem__(self, idx):
-        batch_x = self.x[idx*self.batch_size :
-                        (idx+1)*self.batch_size]
-        batch_y = self.y[idx*self.batch_size :
-                        (idx+1)*self.batch_size]
-
-        return cutmix(batch_x, batch_y)
-
-    
-
-if __name__ == '__main__':
-    data_size = 4
-    x = np.zeros([data_size, 6, 6, 1])
-    y = np.random.randint(4, size=(data_size,))
-    for i, _y in enumerate(y):
-        x[i] = _y
-    y = np.eye(4)[y]
-
-    inputs = tf.keras.layers.Input((x.shape[1:]))
-    _x = tf.keras.layers.Dense(10, activation='relu')(inputs)
-    _x = tf.keras.layers.GlobalMaxPool2D()(_x)
-    _x = tf.keras.layers.Dense(4, activation='softmax')(_x)
-    model = tf.keras.Model(inputs=inputs, outputs=_x)
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=tf.keras.optimizers.Adam())
-       
-    gen = CutmixGen(x, y, batch_size=4)
-    model.fit(gen, epochs=10)
--- Fundus/main.py
+++ Fundus/main.py
@@ -12,11 +12,10 @@ from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from nsml.constants import DATASET_PATH, GPU_NUM
 from keras.utils import np_utils
 from keras import optimizers
-from keras.utils.training_utils import multi_gpu_model
+from keras.preprocessing.image import ImageDataGenerator
 
-from model import cnn_sample, Efficientnet # must be repaired
-from dataprocessing import image_preprocessing, dataset_loader, cutmix_gen, apply_cutmix
-from efficientnet.efficientnet import keras as efn
+from dataprocessing import image_preprocessing, dataset_loader
+from keras.applications.xception import Xception
 
 ## setting values of preprocessing parameters
 RESIZE = 10.
@@ -24,8 +23,7 @@ RESCALE = True
 
 TARGET_RESOLUTION =  int(3900//RESIZE), int(3072//RESIZE)
 
-learning_rate = 1e-4
-
+#learning_rate = 1e-3
 
 def bind_model(model):
     def save(dir_name):
@@ -40,14 +38,14 @@ def bind_model(model):
 
     def infer(data,
               target_resolution=TARGET_RESOLUTION,
-              normalize=RESCALE):  ## test mode
+              ):  ## test mode
         ##### DO NOT CHANGE ORDER OF TEST DATA #####
         X = []
         for i, d in enumerate(data):
             # test 데이터를 training 데이터와 같이 전처리 하기
             X.append(image_preprocessing(d,
                                          target_resolution,
-                                         normalize))
+                                         normalize = True))
         X = np.array(X)
 
         pred = model.predict(X)
@@ -62,7 +60,7 @@ if __name__ == '__main__':
     args = argparse.ArgumentParser()
 
     # hyperparameters
-    args.add_argument('--epoch', type=int, default=45)                          # epoch 수 설정
+    args.add_argument('--epoch', type=int, default=100)                          # epoch 수 설정
     args.add_argument('--batch_size', type=int, default=16)                      # batch size 설정
     args.add_argument('--num_classes', type=int, default=4)                     # DO NOT CHANGE num_classes, class 수는 항상 4
 
@@ -85,20 +83,12 @@ if __name__ == '__main__':
     """ Model """
     # make a model
     h, w = int(3072//RESIZE), int(3900//RESIZE)
-    #model = Efficientnet("efficientnet-b0", in_shape = (h, w, 3), num_classes= 4, pooling = False)
-    model = efn.EfficientNetB2(include_top = True ,input_shape = (h,w,3), classes = 4, weights = None)
-    #model = cnn_sample(in_shape = (h,w,3), num_classes = 4)
-    
-    '''    
-    M = keras.Sequential()
-    M.add(model)
-    M.add(kears.layers.Dense())
-    '''
-    
-    optimizer = optimizers.SGD(lr=learning_rate,
-                               momentum=0.9,
-                               nesterov=True)
+    model = Xception(input_shape=(307, 390, 3), classes=4, include_top=True, weights=None, input_tensor=None, pooling=None)
+    #model = DenseNet169(include_top= True, weights = None, input_shape = (300,300,3), classes = 4)
+    # model = EfficientNetB4(include_top= True, weights = None, input_shape = (307,390,3), classes = 4)
 
+                               
+    optimizer = optimizers.Adam(lr = 1e-5)
 
     model.compile(loss='categorical_crossentropy',
                   optimizer=optimizer,
@@ -122,7 +112,13 @@ if __name__ == '__main__':
         images, labels = dataset_loader(img_path,
                                         TARGET_RESOLUTION,
                                         normalize = RESCALE)
-                                     
+        
+        
+        
+        nsml.load(checkpoint='15', session='team120/KHD2019_FUNDUS/166')
+        nsml.save('best')
+        
+        
         # data 섞기
         total_size = images.shape[0]
         perm = np.random.permutation(total_size)
@@ -130,7 +126,7 @@ if __name__ == '__main__':
         y = labels[perm]
         
         # train, validation split
-        train_val_ratio = 0.8
+        train_val_ratio = 0.7 # 0.8
         tmp = int(total_size*train_val_ratio)
         
         x_train = x[:tmp]
@@ -138,37 +134,34 @@ if __name__ == '__main__':
         y_train = y[:tmp]
         y_val = y[tmp:]
 
-        #dataset = CutmixGen(x_train, y_train, batch_size=8)
+        del x, y, perm
 
         """ Callback """
         
         #callbacks = [ReduceLROnPlateau(monitor='categorical_accuracy', patience=3)]
 
-
-        
         #ModelCheckpoint(file_path, monitor='val_categorical_accuracy', save_best_only=True)
-        
+
+        datagen = ImageDataGenerator(rotation_range= 180 , horizontal_flip= True, vertical_flip= True,  shear_range= 0.2, height_shift_range= 0.2, width_shift_range= 0.2,
+                                    dtype='float32')
+        datagen.fit(x_train)
+    
         """ Training loop """
 
         t0 = time.time()
 
-        best_loss = 1e10
+        best_acc = 1e-10
         for epoch in range(nb_epoch):
             t1 = time.time()
             print("### Model Fitting.. ###")
             print('epoch = {} / {}'.format(epoch+1, nb_epoch))
             print('check point = {}'.format(epoch))
+            
 
-            x_,y_ = apply_cutmix(x_train,y_train, batch_size = batch_size)
-            #x_, y_ = cutmix_gen(x_train, y_train,batch_size = 8)
+            hist = model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),
+                                    steps_per_epoch=x_train.shape[0]//batch_size,
+                                    validation_data=(x_val, y_val))
 
-            #x_, y_ = x_train, y_train
-
-
-            hist = model.fit(x_, y_,validation_data=(x_val, y_val), batch_size=batch_size)
-
-            del x_
-            del y_
 
             t2 = time.time()
             print(hist.history)
@@ -179,10 +172,10 @@ if __name__ == '__main__':
             val_loss = hist.history['val_loss'][0]
 
             # must??
-            best_model = (val_loss < best_loss)
+            best_model = (val_acc > best_acc)
             if best_model:
                 nsml.save('best')
-                best_loss = val_loss
+                best_acc = val_acc
             nsml.report(summary=True, step=epoch, epoch_total=nb_epoch, loss=train_loss, acc=train_acc, val_loss=val_loss, val_acc=val_acc)
             nsml.save(epoch)
         print('Total training time : %.1f' % (time.time() - t0))
